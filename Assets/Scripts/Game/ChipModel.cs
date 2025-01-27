5b54a775904ed6844f04058f1790bcdee4d98167
chuuchuun <allakrylova136@gmail.com>
2025-01-27 13:42:48 +0100

    added chips and raise functionality

---
+++ Assets/Scripts/Game/ChipModel.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ChipColor
+{
+    black,
+    red,
+    green,
+    blue
+}
+public class ChipModel : MonoBehaviour
+{
+    public int value;
+    public ChipColor color;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
